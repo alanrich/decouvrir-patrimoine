7afb37ec9fec94af30ce0598e6c5eb7bae7e3247
alanrich <alan@alanrich.dev>
2024-09-26 20:20:21 +0200

    feat: Add UI components for authentication

--- src/components/MainAppBar/MainAppBar.jsx
+++ src/components/MainAppBar/MainAppBar.jsx
@@ -1,8 +1,36 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Box,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const MainAppBar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogin = () => {
+    handleMenuClose();
+  };
+
+  const handleLogout = () => {
+    handleMenuClose();
+  };
+
   return (
     <AppBar
       position="fixed"
@@ -17,9 +45,38 @@ const MainAppBar = () => {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap component="div">
-          ANABASIS
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{
+            fontWeight: "bold",
+            color: "#D3D3D3",
+          }}
+        >
+          VIGICAM
         </Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        <IconButton color="inherit" onClick={handleMenuOpen} sx={{ p: 0 }}>
+          <AccountCircle fontSize="large" />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleMenuClose}
+          sx={{ mt: 2 }}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          <MenuItem onClick={handleLogin}>Login</MenuItem>
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
