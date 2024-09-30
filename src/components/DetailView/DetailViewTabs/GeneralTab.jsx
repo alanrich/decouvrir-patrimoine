58fa9d94000de4fc32e5a947a2f90b7412ad022e
alanrich <alan@alanrich.dev>
2024-09-30 18:50:01 +0200

    feat: Refactor general tab to dynamically display additional selected data for museums

--- src/components/DetailView/DetailViewTabs/GeneralTab.jsx
+++ src/components/DetailView/DetailViewTabs/GeneralTab.jsx
@@ -7,6 +7,16 @@ const GeneralTab = ({ object }) => {
   }
 
   const { rawData, dataSet } = object;
+  // TODO: determine which data should be excluded from general tab
+  const excludedKeys = [
+    "coordonnees",
+    "adresse",
+    "ville",
+    "nom_officiel",
+    "identifiant",
+    "refmer",
+  ];
+  // TODO: Implement a filter to either remove empty fields or display "no info given"
 
   return (
     <Grid container spacing={2}>
@@ -16,10 +26,10 @@ const GeneralTab = ({ object }) => {
             <Typography variant="h6">{object.name}</Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography variant="body1">Address: {object.address}</Typography>
+            <Typography variant="body1">Adresse: {object.address}</Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography variant="body1">City: {object.city}</Typography>
+            <Typography variant="body1">Ville: {object.city}</Typography>
           </Grid>
           {/* Add more fields as needed */}
         </>
@@ -27,26 +37,41 @@ const GeneralTab = ({ object }) => {
 
       {dataSet === "museums" && (
         <>
+          {/* General Info that Each DataSet ought to display */}
           <Grid item xs={12}>
             <Typography variant="h6">{object.name}</Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography variant="body1">Address: {object.address}</Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              Adresse:{" "}
+            </Typography>
+            <Typography variant="body1">{object.address}</Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography variant="body1">City: {object.city}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="body1">
-              Category: {rawData.categorie}
+            <Typography variant="subtitle1" color="textSecondary">
+              Ville:{" "}
             </Typography>
+            <Typography variant="body1">{object.city}</Typography>
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant="body1">
-              Themes: {rawData.domaine_thematique.join(", ")}
-            </Typography>
-          </Grid>
-          {/* Add more fields as needed */}
+
+          {/* Data Specific to Museum DataSet */}
+          {Object.keys(rawData)
+            .filter((key) => !excludedKeys.includes(key))
+            .map((key) => (
+              <Grid item xs={12} sm={6} key={key}>
+                <Typography variant="subtitle1" color="textSecondary">
+                  {key
+                    .replace(/_/g, " ")
+                    .replace(/\b\w/g, (l) => l.toUpperCase())}
+                  :
+                </Typography>
+                <Typography variant="body1">
+                  {Array.isArray(rawData[key])
+                    ? rawData[key].join(", ")
+                    : rawData[key]}
+                </Typography>
+              </Grid>
+            ))}
         </>
       )}
     </Grid>
