e895c97de1f2c9bd6bdaff17c5f3886a433052fa
alanrich <alan@alanrich.dev>
2024-10-14 12:36:34 +0200

    fix: deleting all heroku git changes

--- Server/server.js
+++ Server/server.js
@@ -1,35 +1,30 @@
+require("dotenv").config();
+
 const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cors = require("cors");
-
-// Load environment variables
-dotenv.config();
-
 const app = express();
-
-// Import models
+const mongoose = require("mongoose");
 const Museum = require("./models/museum");
 const Festival = require("./models/festival");
 
-// MongoDB URI and server port
 const mongoURI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3001;
+const cors = require("cors");
 
+// Re-enable CORS with specific origin
 app.use(
   cors({
-    origin: "http://localhost:3000", // Ensure your domain is correct
+    origin: "https://alanrich.dev", // Allow only your domain
     methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization", "x-requested-with"],
   })
 );
 
-// Field Maps for Sorting
+// Field Maps
 const museumFieldMap = {
   name: "nom_officiel",
   city: "ville",
   genre: "domaine_thematique",
+  // Add other mappings when we decide how else to sort
 };
 
 const festivalFieldMap = {
@@ -50,12 +45,12 @@ app.get("/api/museums", async (req, res) => {
 
   const query = {};
 
-  // Apply filtering
+  // Apply Filtering
   if (searchTerm) {
     query.$text = { $search: searchTerm };
   }
 
-  // Apply sorting
+  // Apply Sorting
   const sort = {};
   if (sortBy) {
     const dbField = museumFieldMap[sortBy];
@@ -63,17 +58,34 @@ app.get("/api/museums", async (req, res) => {
       sort[dbField] = sortOrder === "asc" ? 1 : -1;
     } else {
       console.error(`Invalid sort field: ${sortBy}`);
-      return res.status(400).json({ error: "Invalid sort field" });
+      return res.status(400).json({ error: "Invalid sort field" }); // validation to handle invalid sortBy fields
     }
   }
 
   try {
     const total = await Museum.countDocuments(query);
-    const data = await Museum.find(query)
+    const data = await Museum.find(query, {
+      // We specify the fields to return by adding a projection object
+      identifiant: 1,
+      nom_officiel: 1,
+      adresse: 1,
+      ville: 1,
+      region: 1,
+      coordonnees: 1,
+      url: 1,
+      telephone: 1,
+      categorie: 1,
+      histoire: 1,
+      personnage_phare: 1,
+      interet: 1,
+      annee_creation: 1,
+      domaine_thematique: 1,
+      artiste: 1,
+    })
       .sort(sort)
       .skip(page * rowsPerPage)
       .limit(parseInt(rowsPerPage))
-      .lean();
+      .lean(); // adding lean() to the query chain returns plain js obj instead of mongoose doc for better perf.
 
     res.json({ total, data });
   } catch (error) {
@@ -94,12 +106,12 @@ app.get("/api/festivals", async (req, res) => {
 
   const query = {};
 
-  // Apply filtering
+  // Apply Filtering
   if (searchTerm) {
     query.$text = { $search: searchTerm };
   }
 
-  // Apply sorting
+  // Apply Sorting
   const sort = {};
   if (sortBy) {
     const dbField = festivalFieldMap[sortBy];
@@ -113,7 +125,23 @@ app.get("/api/festivals", async (req, res) => {
 
   try {
     const total = await Festival.countDocuments(query);
-    const data = await Festival.find(query)
+    const data = await Festival.find(query, {
+      identifiant: 1,
+      nom_du_festival: 1,
+      discipline_dominante: 1,
+      commune_principale_de_deroulement: 1,
+      annee_de_creation_du_festival: 1,
+      site_internet_du_festival: 1,
+      envergure_territoriale: 1,
+      adresse_postale: 1,
+      adresse_e_mail: 1,
+      sous_categorie_spectacle_vivant: 1,
+      sous_categorie_musique: 1,
+      sous_categorie_cinema_et_audiovisuel: 1,
+      sous_categorie_arts_visuels_et_arts_numeriques: 1,
+      sous_categorie_livre_et_litterature: 1,
+      geocodage_xy: 1,
+    })
       .sort(sort)
       .skip(page * rowsPerPage)
       .limit(parseInt(rowsPerPage))
@@ -127,11 +155,10 @@ app.get("/api/festivals", async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+app.listen(PORT, () => console.log("Server running on port 3001"));
 // Connect to MongoDB
 mongoose
-  .connect(mongoURI)
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(async () => {
     console.log("MongoDB connected");
 
@@ -140,11 +167,7 @@ mongoose
       await Festival.init();
       console.log("Indexes exist");
     } catch (error) {
-      if (error.code === 85) {
-        console.log("Index already exists with different options, skipping...");
-      } else {
-        console.error("Error initializing indexes:", error);
-      }
+      console.error("Error initializing indexes:", error);
     }
   })
   .catch((err) => console.log("MongoDB connection error:", err));
